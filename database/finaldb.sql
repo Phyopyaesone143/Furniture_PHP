3a07d8a876158b6b01b42f6343895a8320ad1bff
Phyopyaesone143 <phyopyezonwin@gmail.com>
2025-08-30 12:31:06 +0630

    'dbpush'

--- database/finaldb.sql
+++ database/finaldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 11, 2025 at 02:21 AM
+-- Generation Time: Aug 24, 2025 at 03:48 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -32,25 +32,20 @@ CREATE TABLE IF NOT EXISTS `blog` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `image` varchar(200) NOT NULL,
-  `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `description` varchar(200) NOT NULL,
   `currentdate` date NOT NULL,
-  `postuser` varchar(20) NOT NULL,
+  `postuser` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `blog`
 --
 
 INSERT INTO `blog` (`id`, `name`, `image`, `description`, `currentdate`, `postuser`) VALUES
-(9, 'Living Room', 'blog-4.jpg', 'My living room is undoubtedly the favorite room in my house. It is the room where I relax when I am tired. I entertain my friends and any other guests in my living room. My living room is designed in such a way that windows are facing the hills. These hills offer extraordinarily beautiful scenery, and one can just look at them the whole day without noticing how time flies by. At the foot’ of the mountains is incredibly beautiful vegetation comprising mainly of cedar trees and blue lilies. My friends love coming to my home just to have a view of the beautiful snow-capped hills.', '2025-04-04', 'Hein'),
-(6, 'Lover Seat bench together', 'blog-2.jpg', 'A couch, also known as a sofa, settee, chesterfield, or davenport, is a cushioned item of furniture that can seat multiple people. It is commonly found in the form of a bench with upholstered armrests and is often fitted with springs and tailored cushion and pillows.[1][2] Although a couch is used primarily for seating, it may be used for sleeping.[3] In homes, couches are normally put in the fami', '2025-03-29', 'Ko Ko'),
-(7, 'Happy Hour Bubble', 'blog-3.jpg', 'A bathtub, also known simply as a bath or tub, is a container for holding water in which a person or another animal may bathe. Most modern bathtubs are made of thermoformed acrylic, porcelain-enameled steel or cast iron, or fiberglass-reinforced polyester. A bathtub is placed in a bathroom, either as a stand-alone fixture or in conjunction with a shower.\r\nModern bathtubs have overflow and waste dr', '2025-03-29', 'Hein'),
-(10, 'Seat in garden', 'blog-1.jpg', '“A seat in the Garden” by Thomas King, tells the story of the Aboriginal people in Canada today, the stereotypes placed on the people, this story also uses irony to portray the issues and placement of Aboriginals today. At the beginning of the story King draws our attention to the issues of how the Aboriginal people were treated like Canada doesn’t belong to them and they only get to have certain parts of the country after the white people took over their land.', '2025-04-04', 'Pyae'),
-(11, 'Seat at my backyard', 'blog-5.jpg', 'I have picked my backyard because when I was younger it was my favorite place to be at. I used to go out there and play any kind of game, but my favorite thing was playing soccer. My feelings about going out there was something amazing, some other kids would rather play video games and doing some other stuff, but all I wanted to do was to go out in my backyard and just enjoy there. Just like my love for soccer has changed even though I still love it but it is different, and i do not really play out there anymore. Then I started to grow up and the feeling kind of changed, it was not that amazing anymore, but still enjoyed going out there just to play soccer. The more I grew the less excited I was, I knew the feeling was different, but I realized I was growing up.', '2025-04-04', 'Arkar'),
-(12, 'New living room', 'blog-6.jpg', 'The living room is a room that is specifically designed for unwinding and socialization. Let us suppose the living room in your house is messy, cozy, smells of sweat, and has an unattractive look. It is the time where you think that your living room is not designed properly with a good concept. Then, what is a good concept for a living room? A good concept can be anything that can describe the personality of the homeowner, convey a specific message, or follow a particular style. From research and experience, there are several living room concepts which can be used to guide people in designing their living room. When it is all about the living room, it must be a formal and a casual living room. A formal living room is usually designed for a meeting impression with your boss, father or mother-in-law, etc. This type of living room usually has high-standard furniture and decorations. A casual living room is contradicted to the formal living room; it is designed to get a relaxed and comfortable impression with your friends and family, and this one is more flexible to do with any kind of furniture and decoration. From there, it comes a modern and traditional living room. A modern living room is reflecting a simple and clean impression with a minimum amount of furniture. It is suitable for urban people who have a busy life. A modern living room also gives an advanced impression for the homeowner. A traditional living room is more friendly with more furniture and usually has a classic style decoration. This type of living room is more attractive for older people and those who like vintage. And the last is an indoor and an outdoor living room. It is a rare concept, but you can simply differentiate it from the placement.', '2025-04-04', 'admin'),
-(13, 'Dream Workplace', 'computer table.png', 'My dream workplace would preferably be located near a lake and surrounded by trees. I would also like it to be on one of the top floors and, geographically close to everything except, of course, the unnecessary traffic congestion and noise. My office should also have two desks - one would be facing away from a large glass window (from where the lake is visible) so that I could interact with the people who come to visit me. The other desk, on which, I would have my computers, printers, and the other essentials, would be placed against a wall (or maybe even a corner of a small hallway) where the real work would get done. ', '2025-04-04', 'Phway'),
-(15, 'cry', 'blog-3.jpg', 'i wanna it', '2025-07-11', 'Ko Ko');
+(1, 'Happy hour Buddy', 'blog-5.jpg', '“Nothing beats happy hour outdoors 🌿🍹 Relaxing with good vibes, great company, and comfy garden furniture. Perfect way to unwind!”', '2025-08-24', 'admin'),
+(2, 'I Like all from FurnishMe', 'blog-4.jpg', 'The living room is the heart of every home, and the right furniture sets the tone for comfort and style. A cozy sofa, paired with accent chairs, creates a welcoming space for family and friends to gat', '2025-08-24', 'Ko Ko'),
+(3, 'Standard Living Room We got!', 'blog-6.jpg', 'The living room is the heart of every home, and the right furniture sets the tone for comfort and style. A cozy sofa, paired with accent chairs, creates a welcoming space for family and friends to gat', '2025-08-24', 'aungaung');
 
 -- --------------------------------------------------------
 
@@ -61,33 +56,31 @@ INSERT INTO `blog` (`id`, `name`, `image`, `description`, `currentdate`, `postus
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `productname` varchar(200) NOT NULL,
-  `price` bigint NOT NULL,
-  `star` int DEFAULT NULL,
-  `image` varchar(200) DEFAULT NULL,
-  `category` varchar(200) NOT NULL,
+  `productname` varchar(20) NOT NULL,
+  `price` int NOT NULL,
+  `star` int NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `category` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`id`, `productname`, `price`, `star`, `image`, `category`) VALUES
-(16, 'Kid Comfort', 430, 4, 'product-6.png', 'House Sofa'),
-(15, 'Teddy Chair', 345, 5, 'product-5.png', 'House Sofa'),
-(14, 'White Chair', 110, 2, 'product-4.png', 'Office Chair'),
-(12, 'Bunny Sit', 325, 4, 'product-2.png', 'House Sofa'),
-(13, 'Coconut', 280, 4, 'product-3.png', 'House Sofa'),
-(11, 'Wool Stool', 125, 3, 'product-1.png', 'Office Chair'),
-(17, 'Easy chair', 130, 1, 'product-7.png', 'House Sofa'),
-(18, 'Rounded chair', 210, 5, 'product-8.png', 'House Sofa'),
-(19, 'Flower Corner', 365, 4, 'flower corner.png', 'Corner Table'),
-(20, 'Wardrobe for Girls', 555, 5, 'girl cup board.png', 'New Wardrobe'),
-(21, 'Kid Wardrobe', 515, 5, 'kid cupboard.png', 'New Wardrobe'),
-(22, 'Study Table', 985, 5, 'computer table.png', 'Working Table'),
-(23, 'Coding Table', 855, 4, 'working table.png', 'Working Table'),
-(30, 'Hak Hak', 34, 4, 'blog-5.jpg', 'Corner Table');
+(1, 'grounded chair', 523, 2, 'blog-1.jpg', 'House Sofa'),
+(2, 'Stool', 321, 3, 'product-1.png', 'Corner Table'),
+(3, 'Kid Comfort', 345, 4, 'product-2.png', 'House Sofa'),
+(4, 'Coconut', 535, 4, 'product-3.png', 'House Sofa'),
+(5, 'Flexible Chair', 200, 2, 'product-4.png', 'Office Chair'),
+(6, 'Teddy Chair', 675, 5, 'product-5.png', 'House Sofa'),
+(7, 'Relax', 322, 4, 'product-6.png', 'Working Table'),
+(8, 'Mother Choice', 213, 3, 'product-8.png', 'Office Chair'),
+(9, 'Flower Corner', 310, 3, 'flower corner.png', 'Corner Table'),
+(10, 'Sis Wardrobe', 432, 2, 'girl cup board.png', 'New Wardrobe'),
+(11, 'Coding Table', 230, 4, 'computer table.png', 'Working Table'),
+(12, 'Kid Wardrobe', 380, 1, 'kid cupboard.png', 'New Wardrobe');
 
 -- --------------------------------------------------------
 
@@ -103,16 +96,7 @@ CREATE TABLE IF NOT EXISTS `shoppingcart` (
   `price` int NOT NULL,
   `total` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=109 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `shoppingcart`
---
-
-INSERT INTO `shoppingcart` (`id`, `name`, `quantity`, `price`, `total`) VALUES
-(108, 'White Chair', 3, 110, 330),
-(107, 'Teddy Chair', 1, 345, 345),
-(106, 'Kid Comfort', 4, 430, 1720);
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -124,24 +108,24 @@ DROP TABLE IF EXISTS `team`;
 CREATE TABLE IF NOT EXISTS `team` (
   `id` int NOT NULL AUTO_INCREMENT,
   `tname` varchar(50) NOT NULL,
-  `position` varchar(20) NOT NULL,
+  `position` varchar(50) NOT NULL,
   `sarlary` int NOT NULL,
   `image` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=180 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `team`
 --
 
 INSERT INTO `team` (`id`, `tname`, `position`, `sarlary`, `image`) VALUES
-(171, 'Hee San', 'Managing Director', 4520, '790168c96c7d417351b6047e9cbd63ea.jpg'),
-(177, 'Homee Haa', 'Clerk', 7879, ''),
-(178, 'afea jsdf', '098', 809, ''),
-(179, 'afea jsdf', '098', 809, ''),
-(176, 'pu tu', 'Clerk', 2355, ''),
-(151, 'Henry', 'Supervisor', 200, 'team-4.jpg'),
-(148, 'Aunf ducks', 'HR Manager', 4566, 'team-5.jpg');
+(1, 'Jhon', 'CEO', 18000, 'team-6.jpg'),
+(2, 'Nany', 'Managing Director', 15000, 'team-5.jpg'),
+(3, 'Aunf ducks', 'HR Manager', 11500, 'team-3.jpg'),
+(4, 'Jhonny', 'Supervisor', 6000, 'team-2.jpg'),
+(5, 'Peter', 'Operation manager', 11000, 'team-4.jpg'),
+(6, 'Khathy', 'Designer', 14500, 'team-1.jpg'),
+(7, 'U Thaw Sinn San', 'Clerk', 444, 'mom cut.jpg');
 
 -- --------------------------------------------------------
 
@@ -162,14 +146,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`username`, `email`, `password`) VALUES
+('aungaung', 'aa@gmail.com', '$2y$10$u6GSevYIMYAW.4XV1xF28.zzidppid.8pZe1HekWQELervpGa7gPO'),
 ('admin', 'admin@gmail.com', '$2y$10$J0vLC7g70r/H6YwGN8Cks.IXiLp5NmXbUx5En5hg./45l.ehw.EnO'),
-('Arkar', 'arkar@gmail.com', '$2y$10$c3hoZpfERXFb/bOy49RxAulENXxgn0UldRMggyKySdViZkFu4ZL.K'),
-('Hein', 'hein@gmail.com', '$2y$10$PK1cub73tujvbSLZ.HHeN.8ON8YsSLmIYqCFaWyDu1YMUPBlRa7jC'),
-('hnin', 'hnin@gmail.com', '$2y$10$P/E36G.WPHTf..UMNARtGO7XY0TBnW6pJ0NAAmsoYqFGmk9tBMZKC'),
-('Ko Ko', 'kk@gmail.com', '$2y$10$j9gYkDej1h8QC63X8R2lr.OKtf73VwGW8FB/.Qb3/qrv6tXQFoM6C'),
-('Kyaw Kyaw', 'kyaw@gmail.com', '$2y$10$6b6QBaieT48tXNjaP6LLZuPEvov5HhJqos2OMvQIP9Yuht0P1g9NW'),
-('Phway', 'phway@gmail.com', '$2y$10$C4PLCYCuF9AeX86pTIzHpOkqP6n57oY.79AC0pxmYE4lmV/R1Vdle'),
-('Pyae', 'pyae@gmail.com', '$2y$10$B.iu0P0l7UA2k5LIYsML5OMsb1iRVpn.BsOMRLdROarXQt1/iJKpO');
+('July', 'july@gmail.com', '$2y$10$VQIYVJCt/HTxa.ps2cuBP.wfUxGksgSINUnB4sbnEXZ0gRvxCtzOW'),
+('Ko Ko', 'kk@gmail.com', '$2y$10$j9gYkDej1h8QC63X8R2lr.OKtf73VwGW8FB/.Qb3/qrv6tXQFoM6C');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
